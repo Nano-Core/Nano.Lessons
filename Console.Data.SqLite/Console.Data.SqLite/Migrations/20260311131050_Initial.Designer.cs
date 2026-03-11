14a05a99906f81a431b24d1dbfaa1dd7db435f5a
vivet <vivet@vivetonline.com>
2026-03-11 14:43:32 +0100

    Updated

--- Console.Data.SqLite/Console.Data.SqLite/Migrations/20260311131050_Initial.Designer.cs
+++ Console.Data.SqLite/Console.Data.SqLite/Migrations/20260311131050_Initial.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Console.Data.SqLite.Migrations
 {
     [DbContext(typeof(SqLiteDbContext))]
-    [Migration("20260311122957_Initial")]
+    [Migration("20260311131050_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
