ea54630983df948e70d37d465e4d7584a24c8e6d
vivet <vivet@vivetonline.com>
2026-03-12 14:12:41 +0100

    Updated

---
+++ Api.Data.Triggers/Api.Data.Triggers/Controllers/ExamplesController.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Api.Data.SoftDelete.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Nano.App.Api.Controllers;
+using Nano.Data.Abstractions;
+
+namespace Api.Data.SoftDelete.Controllers;
+
+/// <summary>
+/// Controller with examples.
+/// </summary>
+/// <param name="logger">The <see cref="ILogger{T}"/>.</param>
+/// <param name="repository">The <see cref="IRepository"/>.</param>
+public class ExamplesController(ILogger<ExamplesController> logger, IRepository repository)
+    : BaseEntityController(logger, repository)
+{
+    /// <summary>
+    /// No Save Action.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A message.</returns>
+    /// <response code="200">Success.</response>
+    [HttpGet]
+    [Route("no-save")]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    public virtual async Task<IActionResult> NoSaveAsync(CancellationToken cancellationToken = default)
+    {
+        await this.Repository
+            .AddAsync(new Example
+            {
+                Name = "name"
+            }, cancellationToken);
+
+        return this.Ok("no-save");
+    }
+}
---
+++ Api.Data.Triggers/README.md
@@ -0,0 +1,22 @@
+﻿# Api.Data.Triggers
+
+> _Nano API application with data triggers._  
+_All lessons are complete, self-contained examples that include build and deployment setup._
+
+> ⚠️ _To run this solution, the **[Nano.Library](https://github.com/Nano-Core/Nano.Library)** repository must be checked out in the same root directory. 
+Nano is referenced directly from source (not via NuGet packages) and is expected to be located in the .nano solution folder._
+
+> ⚠️ Remember to set the docker-compose project as startup project, before running the solution in Visual Studio.
+
+***
+
+## Table of Contents
+* [Summary](#summary)
+
+## Summary
+This application builds on **[Api.Data.MySql](https://github.com/Nano-Core/Nano.Lessons/tree/master/Api.Data.MySql)**, but any data provider can be used to 
+demonstrate repository autosave. Entity controllers have been simplified to showcase autosave; full controllers are unnecessary.   
+
+
+
+> 📖 Learn more about **[Nano Data Triggers](https://github.com/Nano-Core/Nano.Library/tree/master/Nano.Data#triggers)**.
