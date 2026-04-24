a2a704b1bb646f003b78d9053a7edd55eed1764a
vivet <vivet@vivetonline.com>
2026-04-24 20:40:12 +0200

    updated

---
+++ Api.ApiClients.Audit/Api.Data.Audit.Models/Example.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Nano.Data.Abstractions.Annotations;
+using Nano.Data.Abstractions.Models;
+using Nano.Data.Abstractions.Models.Abstractions;
+
+namespace Api.Data.Audit.Models;
+
+/// <summary>
+/// Example.
+/// </summary>
+public class Example : BaseEntity, IEntityAuditable, IEntitySoftDeletable
+{
+    /// <summary>
+    /// Name.
+    /// </summary>
+    public virtual string Name { get; set; } = null!;
+
+    /// <summary>
+    /// Navigation Id.
+    /// </summary>
+    public virtual Guid? NavigationId { get; set; }
+
+    /// <summary>
+    /// Navigation.
+    /// </summary>
+    [Include]
+    public virtual ExampleNavigation? Navigation { get; set; }
+}
